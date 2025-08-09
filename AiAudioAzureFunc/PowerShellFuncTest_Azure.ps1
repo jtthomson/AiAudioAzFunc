fec2a38ef63e55e6e66b090032f792ac97e5baa4
Jeff <jthomson4u@gmail.com>
2025-08-09 16:02:26 +0000

    Create PowerShellFuncTest_Azure.ps1

---
+++ AiAudioAzureFunc/PowerShellFuncTest_Azure.ps1
@@ -0,0 +1,7 @@
+﻿Invoke-RestMethod -Uri "http://auidioai-fa-c5a0cvcjepgrandy.westus2-01.azurewebsites.net" `
+  -Method POST `
+  -Headers @{ "Content-Type" = "application/json" } `
+  -Body '{
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Tell me a joke"}]
+      }'
